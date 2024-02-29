c4217484357a4e7ceb42dd5c15c799ca4400308a
leadGuitarBG <95097787+BugraUckol@users.noreply.github.com>
2024-03-01 00:10:06 +0300

    [ADD] Propeller calculations are fixed, DJI Tello Drone is added to the database. A basic altitude controller is designed and yaw motion in tested

--- functions/PROPCALC.m
+++ functions/PROPCALC.m
@@ -1,11 +1,11 @@
-function [EN,F,M] = PROPCALC(E,WP,R,C,S,TS)
+function [EN,F,M] = PROPCALC(E,WP,R,C,TS)
 
 % Calculating the force vector
-F = [0, 0, -WP^2 * C(1)]';
+F = [0, 0, -WP^2 * C(1) * sign(WP)]';
 
 % Calculating the moment created by the Aerodynamic drag
 %   S is the sense
-MD = [0, 0, -WP^2 * C(2) * S]';
+MD = [0, 0, -WP^2 * C(2) * sign(WP)]';
 
 % Total moment created about the center of mass
 M = MD + cross(R, F);
--- quad_copter_sim.m
+++ quad_copter_sim.m
@@ -1,21 +1,25 @@
 %% Prep
 % Use dynamics module
 addpath('rigid_body_dynamics\')
+addpath('quadcopter_databse\')
 
 % Start
 clc, clear, close all
 
 % Simulation parameters
-t_s = 0.001;
-t_lim = 50;
+t_s = 0.01;
+t_lim = 100;
+
+% Quad-copter config
+load tello.mat
 
 % Rigid body parameters
-m = 1;
-j = 10 * eye(3);
-j_i = inv(j);
+m = quadcopter.m;
+j = quadcopter.j;
+j_i = quadcopter.j_i;
 
 % Rigid body dimensions
-dims = [5 , 5, 0.5];
+dims = quadcopter.dims;
 
 % Prop Locations
 RP1 = [ dims(1)/2,  dims(2)/2, dims(3)/2]';
@@ -30,7 +34,9 @@ E3 = 0;
 E4 = 0;
 
 % Prop coeffs
-C = [1, 1];
+C = quadcopter.C;
+C_CCW = [-C(1), C(2)];
+C_CW  = [ C(1), C(2)];
 
 % Allocation
 X = zeros(3,t_lim/t_s);
@@ -39,7 +45,7 @@ V = zeros(3,t_lim/t_s);
 W = zeros(3,t_lim/t_s);
 
 % Initial conditions
-X(:,1) = [2, 2, 2]';
+X(:,1) = [2, 2, -2]';
 E(:,1) = [0, 0, 0]';
 V(:,1) = [0, 0, 0]';
 W(:,1) = [0, 0, 0]';
@@ -50,15 +56,19 @@ for i = t_s:t_s:t_lim
 
     step_counter = step_counter + 1;
 
-    WP1 = 1.01;
-    WP2 = 1.01;
-    WP3 = 1;
-    WP4 = 1;
+    err = -5 - X(3,step_counter);
+    errd = V(3, step_counter);
 
-    [E1,F1,M1] = PROPCALC(E1,WP1,RP1,C, 1,t_s);
-    [E2,F2,M2] = PROPCALC(E2,WP2,RP2,C,-1,t_s);
-    [E3,F3,M3] = PROPCALC(E3,WP3,RP3,C, 1,t_s);
-    [E4,F4,M4] = PROPCALC(E4,WP4,RP4,C,-1,t_s);
+    % Propeller angular velocities (CCW : - /// CW : +)
+    WP1 =  1800 / 60 * (2 * pi) * err - 700 * errd;
+    WP2 =  -2000 / 60 * (2 * pi) * err + 700 * errd;
+    WP3 =  1800 / 60 * (2 * pi) * err - 700 * errd;
+    WP4 =  -2000 / 60 * (2 * pi) * err + 700 * errd;
+
+    [E1,F1,M1] = PROPCALC(E1,WP1,RP1,C_CCW,t_s);
+    [E2,F2,M2] = PROPCALC(E2,WP2,RP2,C_CW ,t_s);
+    [E3,F3,M3] = PROPCALC(E3,WP3,RP3,C_CCW,t_s);
+    [E4,F4,M4] = PROPCALC(E4,WP4,RP4,C_CW ,t_s);
     
     F = F1 + F2 + F3 + F4 + 0 * CE2B(E(:,step_counter)) * [0, 0, 9.81]';
     M = M1 + M2 + M3 + M4;
@@ -74,7 +84,6 @@ for i = t_s:t_s:t_lim
 
     if mod(step_counter, 100) == 0
         PLOTOBJECT(3, dims, X_NEW, E_NEW, 1)
-        % pause(0.1)
     end
     
 end
@@ -84,7 +93,7 @@ PLOTRESULTS([1,2],t_s,X,E,V,W)
 
 %% Remove path
 rmpath('rigid_body_dynamics\')
-
+rmpath('quadcopter_databse\')
 
 
 
